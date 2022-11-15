c9bb41d7e203a03da599941d659f7f67bc0f501f
bigvvill <wcurtisusa@gmail.com>
2022-11-15 07:55:37 -0500

    Getting started working on db

---
+++ ShiftTracker/Data/ShiftTrackerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShiftTracker.Models;
+
+namespace ShiftTracker.Data
+{
+    public class ShiftTrackerDbContext : DbContext
+    {
+        public ShiftTrackerDbContext(DbContextOptions<ShiftTrackerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Shift> Shifts { get; set; }
+    }
+}
--- ShiftTracker/Program.cs
+++ ShiftTracker/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ShiftTracker.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("ShiftTrackerDbConnection");
+builder.Services.AddDbContext<ShiftTrackerDbContext>(opt => opt.UseSqlServer(connectionString));
+
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new() { Title = "ShiftTracker", Version = "v1" });
+//});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,6 +25,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ShiftTracker v1"));
 }
 
 app.UseHttpsRedirection();
