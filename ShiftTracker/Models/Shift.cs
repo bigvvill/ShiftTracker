1fa44fffbc844120ca3fc6bc749ccc94681d46db
bigvvill <wcurtisusa@gmail.com>
2022-11-15 23:13:47 -0500

    Database Created

--- ShiftTracker/Models/Shift.cs
+++ ShiftTracker/Models/Shift.cs
@@ -1,12 +1,21 @@
-﻿namespace ShiftTracker.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShiftTracker.Models
 {
     public class Shift
     {
         public int ShiftId { get; set; }
+
         public DateTime Start { get; set; }
+
         public DateTime End { get; set; }
+
+        [Column(TypeName = "decimal(19,4)")]
         public decimal Pay { get; set; }
+
+        [Column(TypeName = "decimal(19,4)")]
         public decimal Minutes { get; set; }
+
         public string Location { get; set; }
     }
 }
